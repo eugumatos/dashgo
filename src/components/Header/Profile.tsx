d767a9446743f46bdbc24484b0461a9d52ef2f20
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-05-25 19:43:36 -0300

    Finished page styles

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,16 @@
+import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
+
+export function Profile() {
+  return (
+    <Flex align="center"> 
+      <Box mr="4" textAlign="right">
+        <Text>Gustavo Matos</Text>
+        <Text color="gray.300" fontSize="small">
+          guamatos@outlook.com
+        </Text>
+      </Box>
+
+      <Avatar size="md" name="Gustavo Matos" src="https://github.com/eugumatos.png" />
+    </Flex>
+  );
+}
